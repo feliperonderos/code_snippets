4dc55c273cdbd98348eebcbc844f9af5fe6e9441
feliperonderos <feliperonderos@gmail.com>
2016-04-16 00:53:05 -0400

    my hola mundo changes

--- BSTree.java
+++ BSTree.java
@@ -3,6 +3,7 @@ package com.company;
 import java.util.ArrayList;
 import java.util.function.Function;
 /**
+ *Lalalalalalal
  * A binary search tree <br>
  * Requires JDK 1.8 for Function*
  * @author Duncan, Felipe Ronderos
